0b20164e5585a01e35aa9770bf5f7061cd28d3bb
Edclydson <edclydson.sousa@gmail.com>
2023-03-03 12:42:41 -0300

    :recycle: refactor(BanklineApplication): Removendo annotation do Swagger 2

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -3,10 +3,8 @@ package com.dio.santander.banklineapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class BanklineApiApplication {
 
 	public static void main(String[] args) {
