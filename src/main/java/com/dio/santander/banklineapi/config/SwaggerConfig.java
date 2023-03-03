a0d328ea5290f38cf4ff914f2f2bdb4d11431b7b
Edclydson <edclydson.sousa@gmail.com>
2023-03-03 12:33:47 -0300

    :books: refactor(SwaggerConfig): Atulizando Bean para OpenAPI

--- src/main/java/com/dio/santander/banklineapi/config/SwaggerConfig.java
+++ src/main/java/com/dio/santander/banklineapi/config/SwaggerConfig.java
@@ -1,22 +1,23 @@
 package com.dio.santander.banklineapi.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-    
     @Bean
-    public Docket docket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build();
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Bankline Api").contact(new Contact().name("Edclydson").url("https://linkedin.com/in/edclydson"))
+                        .description("Api desenvolvida durante a Santander Dev Week - Projeto disponível em: https://github.com/Edclydson/Santander-Dev-Week")
+                        .version("1.0.0"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/edclydson"));
     }
 }
