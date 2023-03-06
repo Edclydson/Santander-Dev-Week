2326f8eba944d140ac3993ed9ca2936a379a9212
Edclydson <edclydson.sousa@gmail.com>
2023-03-06 11:09:46 -0300

    :recycle: refactor(MovimentacaoController): Adicionado throws InsuficientFundsException

--- src/main/java/com/dio/santander/banklineapi/controllers/MovimentacaoController.java
+++ src/main/java/com/dio/santander/banklineapi/controllers/MovimentacaoController.java
@@ -1,21 +1,16 @@
 package com.dio.santander.banklineapi.controllers;
 
-import java.util.List;
-
 import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.exceptions.InsuficientFundsException;
 import com.dio.santander.banklineapi.model.Movimentacao;
-
 import com.dio.santander.banklineapi.services.impl.movimentacao.ListaMovimentacaoImpl;
 import com.dio.santander.banklineapi.services.impl.movimentacao.ListaTodasMovimetacoesImpl;
 import com.dio.santander.banklineapi.services.impl.movimentacao.MovimentacaoImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -41,8 +36,8 @@ public class MovimentacaoController {
 
     @PostMapping
     @Operation(summary = "Realiza uma nova movimentação")
-    public void save(@RequestBody NovaMovimentacao novaMovimentacao){
-        movimentacao.createmovement(novaMovimentacao);
+    public void save(@RequestBody NovaMovimentacao novaMovimentacao) throws InsuficientFundsException {
+        movimentacao.newMovement(novaMovimentacao);
     }
 
     @GetMapping("/{idConta}")
