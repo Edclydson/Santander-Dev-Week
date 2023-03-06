d4b0640af16712b025186023b39f3366709ba550
Edclydson <edclydson.sousa@gmail.com>
2023-03-06 10:53:00 -0300

    :recycle: refactor(IDespesa): Adicionando throws InsuficientFundsException

--- src/main/java/com/dio/santander/banklineapi/services/interfaces/movimentacao/IDespesa.java
+++ src/main/java/com/dio/santander/banklineapi/services/interfaces/movimentacao/IDespesa.java
@@ -1,7 +1,8 @@
 package com.dio.santander.banklineapi.services.interfaces.movimentacao;
 
 import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.exceptions.InsuficientFundsException;
 
 public interface IDespesa{
-    void despesa(NovaMovimentacao movimentacao);
+    void despesa(NovaMovimentacao movimentacao) throws InsuficientFundsException;
 }
